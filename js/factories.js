3d7d0df4b5c3a00e3a10fb312c1b1d372f534098
Jerico <jerico@jsofttechnologies.com>
2015-04-06 17:45:04 +0800

    updates

--- js/factories.js
+++ js/factories.js
@@ -9,7 +9,7 @@ var HOST = "http://localhost:9080/rex-services/";
 angular.module("flowFactories", [])
     .constant("HOST", HOST)
     .constant("VIEWER", "vendors/ViewerJS/#")
-    .constant("REX_VERSION", "1.0")
+    .constant("REX_VERSION", "1.1")
     .constant("FLUID_VERSION", "1.1b");
 
 /*add http://192.168.1.2:9080/rex-war/ when accessing via remote*/
