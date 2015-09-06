989e8a52b2aeec3ac278f5f7dd858d4bcd1f5c98
jsoftgem <jerico@jsofttechnologies.com>
2015-09-06 23:47:19 +0800

    pre for version 0.1.3_2; favicon; ui standardization 87%;

--- src/controller/reports/reports-controller.js
+++ src/controller/reports/reports-controller.js
@@ -106,218 +106,212 @@ angular.module("reportsController", ["fluid", "ngResource", "datatables", "angul
             s.flow.onRefreshed = function () {
                 s.task.query();
             };
-            s.task.load = function () {
+            s.task.newReport = function () {
+                var report = {};
+                report.tag = "all";
+                report.size = 25;
+                report.start = 0;
+                report.isAgent = false;
+                report.isYear = false;
+                report.isMonth = false;
+                report.isRegion = false;
 
-                s.task.newReport = function () {
-                    var report = {};
-                    report.tag = "all";
-                    report.size = 25;
-                    report.start = 0;
-                    report.isAgent = false;
-                    report.isYear = false;
-                    report.isMonth = false;
-                    report.isRegion = false;
+                return report;
+            };
+            s.task.report = s.task.newReport();
 
-                    return report;
-                };
-                s.task.report = s.task.newReport();
+            hp.check("agent", s.task)
+                .success(function (valid) {
+                    s.task.hideAgentFilter = valid;
+                    s.task.report.isAgent = valid;
+                    s.task.report.agent = up.agent;
+                    console.info("profileAgent", up);
+                });
 
-                hp.check("agent", s.task)
-                    .success(function (valid) {
-                        s.task.hideAgentFilter = valid;
-                        s.task.report.isAgent = valid;
-                        s.task.report.agent = up.agent;
-                        console.info("profileAgent", up);
-                    });
+            s.task.change = function () {
+                if (!s.task.report.isYear) {
+                    s.task.report.year = undefined;
+                }
+                if (!s.task.report.isMonth) {
+                    s.task.report.month = undefined;
+                }
+                if (!s.task.report.isAgent) {
+                    s.task.report.agent = undefined;
+                }
+                if (!s.task.report.isRegion) {
+                    s.task.report.region = undefined;
+                }
 
-                s.task.change = function () {
-                    if (!s.task.report.isYear) {
-                        s.task.report.year = undefined;
-                    }
-                    if (!s.task.report.isMonth) {
-                        s.task.report.month = undefined;
-                    }
-                    if (!s.task.report.isAgent) {
-                        s.task.report.agent = undefined;
-                    }
-                    if (!s.task.report.isRegion) {
-                        s.task.report.region = undefined;
-                    }
+            };
 
-                };
+            s.task.query = function () {
+                var url = "services/war/report_weekly_service/agents?";
 
-                s.task.query = function () {
-                    var url = "services/war/report_weekly_service/agents?";
+                var agentId = (s.task.report.agent !== undefined ? s.task.report.agent.id : undefined);
 
-                    var agentId = (s.task.report.agent !== undefined ? s.task.report.agent.id : undefined);
+                var count = 0;
 
-                    var count = 0;
-
-                    if (s.task.report.isYear) {
-                        url += "isYear=true&year=" + s.task.report.year;
-                        count++;
-                    }
-
-
-                    if (s.task.report.isMonth) {
-                        if (count > 0) {
-                            url += "&";
-                        } else {
-                            count++;
-                        }
-                        url += "isMonth=true&month=" + s.task.report.month;
-                    }
-
-                    if (s.task.report.isAgent) {
-                        if (count > 0) {
-                            url += "&"
-                        } else {
-                            count++;
-                        }
-                        url += "isAgent=true&agentId=" + agentId;
-                    }
-
-                    if (s.task.report.isRegion) {
-                        if (count > 0) {
-                            url += "&"
-                        } else {
-                            count++;
-                        }
-                        url += "isRegion=true&region=" + s.task.report.region;
-                    }
-
-
-                    if (s.task.valid() && s.task.report.closed === false) {
-                        if (count > 0) {
-                            url += "&"
-                        }
-                        if (s.task.report.size) {
-                            url += "size=" + s.task.report.size;
-                        }
-                        if (s.task.report.tag) {
-                            url += "&tag=" + s.task.report.tag;
-                        }
-
-                        if (s.task.report.start) {
-                            url += "&start=" + s.task.report.start;
-                        }
-
-                        s.http.get(url).success(function (data) {
-                            s.task.report.size = data.size;
-                            s.task.report.tag = data.tag;
-                            s.task.report.weeklyReports = data.weeklyReports;
-                        })
-                    }
-                };
-
-                s.task.valid = function () {
-                    var valid = true;
-
-                    if (s.task.report.isYear) {
-                        if (s.task.report.year === undefined) {
-                            s.flow.message.danger("Please select a year.");
-                            valid = false;
-                        }
-                    }
-
-
-                    if (s.task.report.isMonth) {
-                        if (s.task.report.month === undefined) {
-                            s.flow.message.danger("Please select a month.");
-                            valid = false;
-                        }
-                    }
-
-                    if (s.task.report.isAgent) {
-                        if (s.task.report.agent === undefined) {
-                            s.flow.message.danger("Please select an agent.");
-                            valid = false;
-                        }
-                    }
-
-                    if (s.task.report.isRegion) {
-                        if (s.task.report.region === undefined) {
-                            s.flow.message.danger("Please select a region.");
-                            valid = false;
-                        }
-                    }
-
-                    return valid;
-
+                if (s.task.report.isYear) {
+                    url += "isYear=true&year=" + s.task.report.year;
+                    count++;
                 }
 
 
+                if (s.task.report.isMonth) {
+                    if (count > 0) {
+                        url += "&";
+                    } else {
+                        count++;
+                    }
+                    url += "isMonth=true&month=" + s.task.report.month;
+                }
+
+                if (s.task.report.isAgent) {
+                    if (count > 0) {
+                        url += "&"
+                    } else {
+                        count++;
+                    }
+                    url += "isAgent=true&agentId=" + agentId;
+                }
+
+                if (s.task.report.isRegion) {
+                    if (count > 0) {
+                        url += "&"
+                    } else {
+                        count++;
+                    }
+                    url += "isRegion=true&region=" + s.task.report.region;
+                }
+
+
+                if (s.task.valid() && s.task.report.closed === false) {
+                    if (count > 0) {
+                        url += "&"
+                    }
+                    if (s.task.report.size) {
+                        url += "size=" + s.task.report.size;
+                    }
+                    if (s.task.report.tag) {
+                        url += "&tag=" + s.task.report.tag;
+                    }
+
+                    if (s.task.report.start) {
+                        url += "&start=" + s.task.report.start;
+                    }
+
+                    s.http.get(url).success(function (data) {
+                        s.task.report.size = data.size;
+                        s.task.report.tag = data.tag;
+                        s.task.report.weeklyReports = data.weeklyReports;
+                    })
+                }
             };
 
-            s.task.postLoad = function () {
-                s.$watch(function (scope) {
-                    return scope.task.report.isYear
-                }, function (newValue, oldValue) {
-                    if (newValue !== oldValue) {
-                        s.task.change();
-                    }
-                });
+            s.task.valid = function () {
+                var valid = true;
 
-                s.$watch(function (scope) {
-                    return scope.task.report.isMonth
-                }, function (newValue, oldValue) {
-                    if (newValue !== oldValue) {
-                        s.task.change();
+                if (s.task.report.isYear) {
+                    if (s.task.report.year === undefined) {
+                        s.flow.message.danger("Please select a year.");
+                        valid = false;
                     }
-                });
+                }
 
-                s.$watch(function (scope) {
-                    return scope.task.report.isAgent
-                }, function (newValue, oldValue) {
-                    if (newValue !== oldValue) {
-                        s.task.change();
-                    }
-                });
 
-                s.$watch(function (scope) {
-                    return scope.task.report.isRegion
-                }, function (newValue, oldValue) {
-                    if (newValue !== oldValue) {
-                        s.task.change();
+                if (s.task.report.isMonth) {
+                    if (s.task.report.month === undefined) {
+                        s.flow.message.danger("Please select a month.");
+                        valid = false;
                     }
-                });
+                }
 
-                s.$watch(function (scope) {
-                    return scope.task.report.year
-                }, function (newValue, oldValue) {
-                    if (newValue !== oldValue) {
-                        s.task.report.closed = false;
-                        s.task.query();
+                if (s.task.report.isAgent) {
+                    if (s.task.report.agent === undefined) {
+                        s.flow.message.danger("Please select an agent.");
+                        valid = false;
                     }
-                })
+                }
 
-                s.$watch(function (scope) {
-                    return scope.task.report.month
-                }, function (newValue, oldValue) {
-                    if (newValue !== oldValue) {
-                        s.task.report.closed = false;
-                        s.task.query();
+                if (s.task.report.isRegion) {
+                    if (s.task.report.region === undefined) {
+                        s.flow.message.danger("Please select a region.");
+                        valid = false;
                     }
-                });
+                }
 
-                s.$watch(function (scope) {
-                    return scope.task.report.agent
-                }, function (newValue, oldValue) {
-                    if (newValue !== oldValue) {
-                        s.task.report.closed = false;
-                        s.task.query();
-                    }
-                });
+                return valid;
 
-                s.$watch(function (scope) {
-                    return scope.task.report.region
-                }, function (newValue, oldValue) {
-                    if (newValue !== oldValue) {
-                        s.task.report.closed = false;
-                        s.task.query();
-                    }
-                })
-            };
+            }
+
+            s.$watch(function (scope) {
+                return scope.task.report.isYear
+            }, function (newValue, oldValue) {
+                if (newValue !== oldValue) {
+                    s.task.change();
+                }
+            });
+
+            s.$watch(function (scope) {
+                return scope.task.report.isMonth
+            }, function (newValue, oldValue) {
+                if (newValue !== oldValue) {
+                    s.task.change();
+                }
+            });
+
+            s.$watch(function (scope) {
+                return scope.task.report.isAgent
+            }, function (newValue, oldValue) {
+                if (newValue !== oldValue) {
+                    s.task.change();
+                }
+            });
+
+            s.$watch(function (scope) {
+                return scope.task.report.isRegion
+            }, function (newValue, oldValue) {
+                if (newValue !== oldValue) {
+                    s.task.change();
+                }
+            });
+
+            s.$watch(function (scope) {
+                return scope.task.report.year
+            }, function (newValue, oldValue) {
+                if (newValue !== oldValue) {
+                    s.task.report.closed = false;
+                    s.task.query();
+                }
+            })
+
+            s.$watch(function (scope) {
+                return scope.task.report.month
+            }, function (newValue, oldValue) {
+                if (newValue !== oldValue) {
+                    s.task.report.closed = false;
+                    s.task.query();
+                }
+            });
+
+            s.$watch(function (scope) {
+                return scope.task.report.agent
+            }, function (newValue, oldValue) {
+                if (newValue !== oldValue) {
+                    s.task.report.closed = false;
+                    s.task.query();
+                }
+            });
+
+            s.$watch(function (scope) {
+                return scope.task.report.region
+            }, function (newValue, oldValue) {
+                if (newValue !== oldValue) {
+                    s.task.report.closed = false;
+                    s.task.query();
+                }
+            })
+
 
             s.task.page.load = function () {
                 if (this.name === s.task.pageAgent) {
