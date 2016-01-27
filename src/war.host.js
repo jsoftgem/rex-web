62f62f39262632e9b1ec33480d4e2212765ad4a7
Jerico <jerico@jsofttechnologies.com>
2016-01-27 11:46:02 +0800

    code update https://github.com/jsoftgem/rex-web/issues/38

--- src/war.host.js
+++ src/war.host.js
@@ -1,4 +1,4 @@
-var HOST = "http://localhost:9080/rex-services/";
+var HOST = "http://war.rexpublishing.com.ph:8080/rex-services/";
 
 function withHost(url) {
     if (url && url.charAt(0) === '/') {
